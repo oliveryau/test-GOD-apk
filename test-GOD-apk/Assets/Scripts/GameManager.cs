b727840c485a51ee1cfdaf68d25405c9a6a8b23a
Oliver Yau <55869156+oliveryau@users.noreply.github.com>
2026-04-17 00:03:36 +0800

    testing 4x4 grid instantiation and cam angles

---
+++ test-GOD-apk/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _width = 4;
+    [SerializeField] private int _depth = 4;
+    [SerializeField] private Box _boxPrefab;
+
+    private void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int z = 0; z < _depth; z++)
+            {
+                var box = Instantiate(_boxPrefab, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+}
