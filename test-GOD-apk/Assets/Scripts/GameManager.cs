f57dd4a41570f2e174c8449866884da0d4470a31
oliveryau <oliveryau0907@gmail.com>
2026-04-22 18:05:42 +0800

    merging

--- test-GOD-apk/Assets/Scripts/GameManager.cs
+++ test-GOD-apk/Assets/Scripts/GameManager.cs
@@ -305,6 +305,7 @@ public class GameManager : MonoBehaviour
         Units spawned = Instantiate(prefab, spawnPosition, Quaternion.identity);
         spawned.Setup(this, tier, spawnPoint);
         spawned.transform.rotation = Quaternion.Euler(_unitFacingEuler);
+        if (tier > 1 && spawned.AudioSource != null) spawned.AudioSource.PlayOneShot(spawned.UnitMergeSfx);
 
         if (spawnPoint != null)
         {
